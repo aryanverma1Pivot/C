ff000c269b0f08a4c6282a3c00ea61a9350dbde7
Apple <apple@Aryans-MacBook-Air.local>
2024-09-09 23:43:13 +0530

    Changed few mistakes in sum and wrote comments for Temp

--- sum.c
+++ sum.c
@@ -4,10 +4,18 @@ int main(){
     // Variable declaration
     int num1,num2, sum;
     // INput
-    printf("Enter num1 and num2 :");
-    scanf("%d %d",num1,num2);
+    printf("Enter num1 and num2  :\n");
+    scanf("%d %d",&num1,&num2);
     // logic
     sum=num1+num2;
+    printf("Sum %d",sum);
     // colcude
     return 0;
 }
+
+// This question made clear about format specifiers in c and how they're used
+/*
+I made some mistakes had erros but eventually learned that we don't 
+Have to put too much spaces because it can have handeling issues. Also learnt
+About how to use 
+*/
