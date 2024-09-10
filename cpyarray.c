aca68a4a62e05e683b65c6a6ac7ca3dba8d60b0e
Apple <apple@Aryans-MacBook-Air.local>
2024-09-11 02:16:16 +0530

    Errors not fixed

---
+++ cpyarray.c
@@ -0,0 +1,20 @@
+/* Write a program to copy array into a new array individually*/
+#include<stdio.h>
+int main (){
+    int arr[5]={1,2,3,4,5};
+    int cpy[5];
+
+
+        NOT SOLVED.  NOT SOLVED.  NOT SOLVED  
+
+    for (int i=0;i<5;i++){
+        arr[i]=cpy[i];// This take the indexes of arr and cpy. And then equls 0th value 
+        // of arr to cpy. Hence 0th element of cpy will be 1. 
+        printf("arr=%d\n",arr);
+    }
+        return 0;
+}
+
+// Do not forget to initilise every variable you use
+// Keep in mind those syntaxes specilly-->  ; 
+// Do not forget format specifier
