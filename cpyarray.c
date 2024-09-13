1fed202e95abbda39468dc1d37018653ba291a4d
Apple <apple@Aryans-MacBook-Air.local>
2024-09-13 09:40:14 +0530

    Error Fixed-- Arrays working

--- cpyarray.c
+++ cpyarray.c
@@ -1,20 +1,21 @@
-/* Write a program to copy array into a new array individually*/
-#include<stdio.h>
-int main (){
-    int arr[5]={1,2,3,4,5};
+/* Write a program to copy array into a new array individually */
+#include <stdio.h>
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
     int cpy[5];
 
+    for (int i = 0; i < 5; i++)
+    {
+        int temp = arr[i]; // This take the indexes of arr and cpy. And then equls 0th value
+        cpy[i] = temp;
+        // of arr to cpy. Hence 0th element of cpy will be 1.
 
-        NOT SOLVED.  NOT SOLVED.  NOT SOLVED  
-
-    for (int i=0;i<5;i++){
-        arr[i]=cpy[i];// This take the indexes of arr and cpy. And then equls 0th value 
-        // of arr to cpy. Hence 0th element of cpy will be 1. 
-        printf("arr=%d\n",arr);
+        printf("arr=%d\n", cpy[i]);
     }
-        return 0;
+    return 0;
 }
 
 // Do not forget to initilise every variable you use
-// Keep in mind those syntaxes specilly-->  ; 
+// Keep in mind those syntaxes specilly-->  ;
 // Do not forget format specifier
