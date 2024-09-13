e0b647a23396b71f4039f1fab03b99642e6133db
Apple <apple@Aryans-MacBook-Air.local>
2024-09-14 03:12:47 +0530

    Error Fixed in Matrix addition

--- Matrix_add.c
+++ Matrix_add.c
@@ -2,61 +2,70 @@
 #include <stdio.h>
 #define row 10
 #define col 10
-int main(){
+int main()
+{
     // Read two matrices
-    int M1 [row][col], M2[row][col],M3[row][col], i,j;
+    int M1[row][col], M2[row][col], M3[row][col], i, j;
     int row1, col1, row2, col2;
 
+    // Asking How many row/ col Do you want to perfrom for
     printf("Enter row value for M1\n");
-    scanf("%d", row1);
-    we can do pre
+    scanf("%d", &row1);
     printf("Enter col value for M1\n");
-    scanf("%d", col1);
+    scanf("%d", &col1);
 
-    prinf("Enter row value for M2\n");
-    prinf("Enter col value for M2\n");
-    scanf("%d", col2);
-    scanf("%d", row2);
+    printf("Enter row value for M2\n");
+    scanf("%d", &row2);
+    printf("Enter col value for M2\n");
+    scanf("%d", &col2);
     // Check
 
-    if (row1 != col1 || row2 != col2){
-        prinf("Invalid Matrix\n");
-        return;
+    if (row1 != row2 || col1 != col2)
+    {
+        printf("Invalid Matrix\n");
+        return 0;
     }
 
     // Take inputs for Matrix 1
 
     printf("Enter values for matrix 1\n");
-    for (i=0;i<row1;i++){
-        for (j=0;j<col1;j++){
+    for (i = 0; i < row1; i++)
+    {
+        for (j = 0; j < col1; j++)
+        {
             scanf("%d", &M1[i][j]);
         }
-        printf("\n");
-
+        printf("\n"); // Just to print new-line character
     }
-// This Takes the input value for matrix 2
+    // This Takes the input value for matrix 2
     printf("Enter values for matrix 2\n");
-    for (i=0;i<row2;i++){
-        for (j=0;j<col2;j++){
+    for (i = 0; i < row2; i++)
+    {
+        for (j = 0; j < col2; j++)
+        {
             scanf("%d", &M2[i][j]);
         }
         printf("\n");
+    }
 
-    }   
+    // Here we write logic of the code
 
-// Here we write logic of the code 
-
-    for (i=0;i<row1;i++){
-        for (j=0;j<row1;j++){
-            M3[i][j]=M1[i][j]+M2[i][j];
+    for (i = 0; i < row1; i++)
+    {
+        for (j = 0; j < col1; j++)
+        {
+            M3[i][j] = M1[i][j] + M2[i][j];
         }
     }
     // Now print the matrix
-    for(i=0;i<row1;i++){
-        for (j=0;j<row1;j++){
-            printf("%d",&M3[i][j]);// printf the address (&) of M i j
-        
+    for (i = 0; i < row1; i++)
+    {
+        for (j = 0; j < col1; j++)
+        {
+            printf("%d ", M3[i][j]); // printf the address (&) of M i j
         }
         printf("\n");
     }
 }
+
+// We don't use & address off in the printf statement
--- selection_sort.c
+++ selection_sort.c
@@ -1,30 +1,39 @@
 /*
-Write the implementation of selection sort algorithm. 
+Write the implementation of selection sort algorithm.
 */
-#include<stdio.h>
-void Selection_sort(int arr[], int n){
-    // We have to go to every index 
-    for(int i=0;i<n;i++){
-         // check for minumun among unsorted array
+#include <stdio.h>
+void Selection_sort(int arr[], int n)
+{
+    // We have to go to every index
+    for (int i = 0; i < n; i++)
+    {
+        // check for minumun among unsorted array
 
-        int min=i;
-        for(int j=i+1;j<n;j++){
-            if(arr[min]>arr[j]){
+        int min;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[i])
                 min = j;
-            }
         }
         // swap the elements
-            int temp = arr[min]; // Here we're out of the looop. so cannot use j. we can use min. 
-            // Above we're storing value of arr[min] into tempt
-            arr[min]=arr[i];// here we're storing value of arr[i] into arr[min]
-            arr[i] =temp; // here we're storing value of temp into arr[i] position
-    }   
+        int temp = arr[min]; // Here we're out of the looop. so cannot use j. we can use min.
+        // Above we're storing value of arr[min] into tempt
+        arr[min] = arr[i]; // here we're storing value of arr[i] into arr[min]
+        arr[i] = temp;     // here, we're storing value of temp into arr[i] position
+    }
 }
 // main function consisting of arr, n and then pass the function.
-int main (){
-    int arr[]={3,5,7,8,2};
-    int size=sizeof (arr) / sizeof(arr[0]);
-    Selection_sort(arr, size); // Here we're passing arr and size as a parameter
-    // printf("Sorted array is: %d", sort);
+int main()
+{
+    int a[] = {3, 5, 7, 8, 2};
+    int size = sizeof(a) / sizeof(a[0]);
+    Selection_sort(a, size); // Here we're passing arr and size as a parameter
+    // Print the sorted array
+    printf("Sorted array is: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n"); // To ensure the output ends with a new line
     return 0;
 }
